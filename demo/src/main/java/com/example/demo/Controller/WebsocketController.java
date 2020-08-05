ae7b77d2eaf6c09a140a1cfd5173a104cf5b54c2
karthikeyan kesavan <karthikeyan.kesavanr@gmail.com>
2020-08-05 20:22:28 +0530

    websocket controller

---
+++ demo/src/main/java/com/example/demo/Controller/WebsocketController.java
@@ -0,0 +1,44 @@
+package com.example.demo.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Service.UserService;
+import com.example.demo.model.MessagingVO;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+@RestController
+public class WebsocketController {
+	
+	private final SimpMessagingTemplate template;
+	
+	@Autowired
+	UserService userService;
+
+	@Autowired
+	WebsocketController(SimpMessagingTemplate template) {
+		this.template = template;
+	}
+	
+	@MessageMapping("/send/message")
+	public void sendMessage(@Payload MessagingVO message) {
+		// {"text":null,"sender_id":0,"receiver_id":0,"chat_id":0,"msg_Type":0,"groupChatId":null,"singleChatId":null}
+		userService.sendMsg(message);
+		JsonParser jsonParser = new JsonParser();
+		JsonElement Elem = jsonParser.parse(userService.getConversation(message.getChat_id()));
+		JsonArray JsonArray = Elem.getAsJsonObject().get("Conversation").getAsJsonObject().get("chats")
+				.getAsJsonArray();
+		JsonElement lastElem = JsonArray.get(JsonArray.size() - 1);
+		this.template.convertAndSend("/message", lastElem.toString());
+	}
+	
+	@MessageMapping("/send/typing")
+	public void sendMessage(@Payload String typing) {
+		this.template.convertAndSend("/typing", typing.toString());
+	}
+}
